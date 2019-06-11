520f9b62043bb9192b52d29817a229c2c0eb638a
Svaity <43662680+Svaity@users.noreply.github.com>
2019-06-11 17:24:53 -0400

    First commit

---
+++ petadoption/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.conf.urls import url
+#from django.urls import path
+
+from adoptions import views
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoptions/(\d+)/', views.pet_detail, name='pet_detail'),
+]
